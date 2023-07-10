af20bc71d6415dddc841b6660b4e1ede0b4b7589
Pranil01 <94604003+Pranil01@users.noreply.github.com>
2023-07-10 23:34:50 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Pranil Kalyani");
+
+int a =4;
+	int b = 6;
+       int result = a+b;
+
+Console.WriteLine("result is :"+" "+result);
